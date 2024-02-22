bb874fe3b7c07d3b4cd720bfd65604fa0bb34392
Fillah Amanda Sulton <2131710001@student.polinema.ac.id>
2024-02-22 16:03:36 +0700

    Meneruskan data ke view

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,6 +11,8 @@ class WelcomeController extends Controller
     }
 
     public function greeting(){
-        return view('blog.hello', ['name' => 'Fillah']);
+        return view('blog.hello')
+        ->with('name', 'Fillah')
+        ->with('occupation', 'Mobile Developer');
     }
 }
